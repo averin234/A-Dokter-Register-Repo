ed2b5c52db3693fd76a71d674a0e8072a2d021c4
Irwan Setiawan <irwanstwn2398@gmail.com>
2023-07-08 22:08:47 +0700

    ngerapihin doang

--- lib/app/modules/detail_antrian/views/componen/planning/resep.dart
+++ lib/app/modules/detail_antrian/views/componen/planning/resep.dart
@@ -1,6 +1,12 @@
 import 'package:a_dokter_register/app/data/model/get_detail_mr.dart';
 import 'package:a_dokter_register/app/modules/detail_antrian/views/componen/list_resep.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../../../../../data/componen/fetch_data.dart';
+import '../../../../../routes/app_pages.dart';
+import '../../../../detail_riwayat/controllers/detail_riwayat_controller.dart';
 
 class PlanningResep extends StatelessWidget {
   final List<Resep> resep;
@@ -8,6 +14,7 @@ class PlanningResep extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(DetailRiwayatController());
     return Container(
       margin: const EdgeInsets.only(right: 10, left: 10),
       padding: const EdgeInsets.only(right: 10, left: 10, bottom: 10),
@@ -47,27 +54,43 @@ class PlanningResep extends StatelessWidget {
               const SizedBox(
                 height: 10,
               ),
-              Container(
-                height: 30,
-                width: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  // boxShadow: [
-                  //   BoxShadow(
-                  //     color: Colors.blue.withOpacity(0.5),
-                  //     spreadRadius: 0,
-                  //     blurRadius: 10,
-                  //     offset: const Offset(2, 1),
-                  //   ),
-                  // ],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    // Text("Print",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 13),)
-                  ],
-                ),
-              )
+              // GestureDetector(
+              //   onTap: () async {
+              //     final cetakResep = await API.cetakResep(
+              //         no_registrasi: controller.noRegistrasi);
+              //     print('resep : $cetakResep');
+              //     Get.toNamed(Routes.CETAKAN, arguments: {'file': cetakResep});
+              //   },
+              //   child: Container(
+              //     height: 30,
+              //     width: 120,
+              //     decoration: BoxDecoration(
+              //       color: Colors.green,
+              //       borderRadius: BorderRadius.circular(10),
+              //       boxShadow: [
+              //         BoxShadow(
+              //           color: Colors.green.withOpacity(0.5),
+              //           spreadRadius: 0,
+              //           blurRadius: 10,
+              //           offset: const Offset(2, 1),
+              //         ),
+              //       ],
+              //     ),
+              //     child: Column(
+              //       mainAxisAlignment: MainAxisAlignment.center,
+              //       children: const [
+              //         Text(
+              //           "Print",
+              //           style: TextStyle(
+              //               color: Colors.white,
+              //               fontWeight: FontWeight.bold,
+              //               fontSize: 13),
+              //         )
+              //       ],
+              //     ),
+              //   ),
+              // ),
+
             ],
           ),
           Column(
--- lib/app/modules/isi_resep/views/componen/hasil_resep.dart
+++ lib/app/modules/isi_resep/views/componen/hasil_resep.dart
@@ -12,7 +12,7 @@ class HasilResep extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         padding: const EdgeInsets.all(10),
-        margin: const EdgeInsets.only(left: 10, right: 10),
+        margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(10),
